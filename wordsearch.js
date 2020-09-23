1be7933f2d4fa936b24c3232abcbb8f037f1c3c6
David Folkins <david.folkins88@gmail.com>
2020-09-23 18:50:12 -0400

    wordsearch now works for horizontal and vertical matrix

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
 
-module.exports = wordSearch
+  let numColumns = matrix.length;
+  let numRows = matrix[0].length;
+  let output = [];
+
+  for (let row = 0; row < numRows; row++) {
+    let temp = [];
+
+    for (let column = 0; column < numColumns; column++) {
+      temp.push(matrix[column][row]);
+    }
+    output.push(temp);
+  }
+  return output;
+};
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (let h of horizontalJoin) {
+    if (h.includes(word)) {
+      return true;
+    }
+  }
+  const verticalSearch = transpose(letters).map(ls => ls.join(''));
+
+  for (let v of verticalSearch) {
+    if (v.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+module.exports = wordSearch;
